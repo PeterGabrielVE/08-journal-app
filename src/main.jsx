bbcc80534c662176076d4b2e8904d99c3f6aec84
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-10-25 12:13:27 -0400

    ajuste

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,15 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { store } from './store';
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+     <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
